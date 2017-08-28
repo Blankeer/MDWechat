0c5ef5a7b7c7d0f5d398db3a4fb0449d7282e24a
Blanke <blankeeee@gmail.com>
2017-08-29 00:37:30 +0800

    [ADD] add conversation ripple.

--- app/src/main/java/com/blanke/mdwechat/ui/ConversationHook.java
+++ app/src/main/java/com/blanke/mdwechat/ui/ConversationHook.java
@@ -2,13 +2,12 @@ package com.blanke.mdwechat.ui;
 
 import android.graphics.drawable.Drawable;
 import android.view.View;
+import android.view.ViewGroup;
 
 import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
 
-import static com.blanke.mdwechat.WeChatHelper.wxConfig;
 import static com.blanke.mdwechat.WeChatHelper.xMethod;
-import static de.robv.android.xposed.XposedHelpers.getObjectField;
 
 /**
  * Created by blanke on 2017/8/1.
@@ -17,13 +16,22 @@ import static de.robv.android.xposed.XposedHelpers.getObjectField;
 public class ConversationHook extends BaseHookUi {
     @Override
     public void hook(XC_LoadPackage.LoadPackageParam lpparam) {
-        xMethod(wxConfig.classes.ConversationFragment,
-                wxConfig.methods.MainFragment_onTabCreate,
+//        xMethod(wxConfig.classes.ConversationFragment,
+//                wxConfig.methods.MainFragment_onTabCreate,
+//                new XC_MethodHook() {
+//                    @Override
+//                    protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+//                        View listView = (View) getObjectField(param.thisObject, wxConfig.fields.ConversationFragment_mListView);
+////                        listView.setBackground(getBackground());
+//                    }
+//                });
+        xMethod("com.tencent.mm.ui.conversation.g",
+                "getView", int.class, View.class, ViewGroup.class,
                 new XC_MethodHook() {
                     @Override
                     protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-                        View listView = (View) getObjectField(param.thisObject, wxConfig.fields.ConversationFragment_mListView);
-//                        listView.setBackground(getBackground());
+                        View view = (View) param.getResult();
+                        view.setBackground(getRippleDrawable(view.getContext()));
                     }
                 });
     }
