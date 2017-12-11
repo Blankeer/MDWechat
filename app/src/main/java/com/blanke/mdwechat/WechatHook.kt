1fe818d0de7b295640e19ffbb7a1cbcb67df9da0
Blanke <blankeeee@gmail.com>
2017-12-11 12:52:42 +0800

    [REM] remove no use code.

--- app/src/main/java/com/blanke/mdwechat/WechatHook.kt
+++ app/src/main/java/com/blanke/mdwechat/WechatHook.kt
@@ -1,22 +1,14 @@
 package com.blanke.mdwechat
 
 import android.content.Context
-import android.content.res.XModuleResources
 import com.blanke.mdwechat.config.WxObjects
 import com.blanke.mdwechat.util.LogUtil.log
-import de.robv.android.xposed.*
-import de.robv.android.xposed.callbacks.XC_InitPackageResources
+import de.robv.android.xposed.IXposedHookLoadPackage
+import de.robv.android.xposed.XposedHelpers
 import de.robv.android.xposed.callbacks.XC_LoadPackage
 import java.lang.ref.WeakReference
 
-class WechatHook : XC_MethodHook(), IXposedHookZygoteInit, IXposedHookLoadPackage, IXposedHookInitPackageResources {
-    private lateinit var MODULE_PATH: String
-
-    @Throws(Throwable::class)
-    override fun initZygote(startupParam: IXposedHookZygoteInit.StartupParam) {
-        MODULE_PATH = startupParam.modulePath
-    }
-
+class WechatHook : IXposedHookLoadPackage {
     @Throws(Throwable::class)
     override fun handleLoadPackage(lpparam: XC_LoadPackage.LoadPackageParam) {
         if (lpparam.packageName != Common.WECHAT_PACKAGENAME) {
@@ -38,40 +30,4 @@ class WechatHook : XC_MethodHook(), IXposedHookZygoteInit, IXposedHookLoadPackag
             log(e)
         }
     }
-
-    private fun hookSettings(lpparam: XC_LoadPackage.LoadPackageParam, versionName: String, isSupport: Boolean) {
-        XposedHelpers.findAndHookMethod("com.blanke.mdwechat.settings.SettingsFragment",
-                lpparam.classLoader, "getWeChatVersion", object : XC_MethodHook() {
-            @Throws(Throwable::class)
-            override fun beforeHookedMethod(param: XC_MethodHook.MethodHookParam?) {
-                param!!.result = versionName
-            }
-        })
-        XposedHelpers.findAndHookMethod("com.blanke.mdwechat.settings.SettingsFragment",
-                lpparam.classLoader, "isSupportWechat", object : XC_MethodHook() {
-            @Throws(Throwable::class)
-            override fun beforeHookedMethod(param: XC_MethodHook.MethodHookParam?) {
-                param!!.result = isSupport
-            }
-        })
-
-    }
-
-    @Throws(Throwable::class)
-    override fun handleInitPackageResources(resparam: XC_InitPackageResources.InitPackageResourcesParam) {
-        if (resparam.packageName != Common.WECHAT_PACKAGENAME) {
-            return
-        }
-        //        log("handleInitPackageResources");
-        val modRes = XModuleResources.createInstance(MODULE_PATH, resparam.res)
-        //        WeChatHelper.WCId.ic_add = resparam.res.addResource(modRes, R.drawable.ic_add);
-
-
-        //        resparam.res.setReplacement(Common.WECHAT_PACKAGENAME,
-        //                "color", "z", Color.RED);
-        //        resparam.res.setReplacement(Common.WECHAT_PACKAGENAME,
-        //                "drawable", WeChatHelper.WCDrawable.Conference_ListView_Item_Background,
-        //                modRes.fwd(R.drawable.selector_item));
-    }
-
 }
