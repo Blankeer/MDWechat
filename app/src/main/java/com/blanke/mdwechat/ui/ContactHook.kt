74272c82a8a7dfce6e67d52a03a0ba2cf7ce0c19
Blanke <blankeeee@gmail.com>
2018-03-09 22:24:50 +0800

    [FIX] 修复企业号背景不透明的问题

--- app/src/main/java/com/blanke/mdwechat/ui/ContactHook.kt
+++ app/src/main/java/com/blanke/mdwechat/ui/ContactHook.kt
@@ -4,6 +4,7 @@ import android.graphics.drawable.BitmapDrawable
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ListView
+import android.widget.TextView
 import com.blanke.mdwechat.WeChatHelper.wxConfig
 import com.blanke.mdwechat.WeChatHelper.xMethod
 import com.blanke.mdwechat.config.AppCustomConfig
@@ -39,22 +40,37 @@ class ContactHook : BaseHookUi() {
                         if (HookConfig.is_hook_ripple) {
                             if (listView.headerViewsCount > 0) {
                                 val mHeaderViewInfos = getObjectField(listView, "mHeaderViewInfos") as ArrayList<ListView.FixedViewInfo>
-                                val header = mHeaderViewInfos[0].view
-                                if (header != null) {
-//                                    printViewTree(header, 0)
-                                    val headLayout = header as ViewGroup
-                                    for (i in 0..headLayout.childCount - 1) {
-                                        val item = headLayout.getChildAt(i) as ViewGroup
-                                        if (item.childCount == 0) {
-                                            continue
-                                        }
-                                        val itemContent = item.getChildAt(0)
-//                                        headViews.put(i, itemContent)
-                                        itemContent.background = getTransparentRippleDrawable()
-                                        if (itemContent != null) {
-                                            val childView = (itemContent as ViewGroup).getChildAt(0)
-                                            if (childView != null) {
-                                                childView.background = transparentDrawable
+                                for (j in 0 until mHeaderViewInfos.size) {
+                                    val header = mHeaderViewInfos[j].view
+                                    if (header != null) {
+//                                        printViewTree(header, 0)
+                                        if (header is ViewGroup) {
+                                            val headLayout = header
+                                            for (i in 0 until headLayout.childCount) {
+                                                val item = headLayout.getChildAt(i)
+                                                if (item !is ViewGroup || item.childCount == 0) {
+                                                    continue
+                                                }
+                                                val itemContent = item.getChildAt(0)
+                                                if (itemContent != null) {
+                                                    // 新的朋友 等几个 item
+                                                    itemContent.background = getTransparentRippleDrawable()
+                                                    if (itemContent is ViewGroup) {
+                                                        val childView = itemContent.getChildAt(0)
+                                                        childView.background = transparentDrawable
+                                                        if (childView is TextView) {// 企业号
+                                                            itemContent.background = transparentDrawable
+                                                            val lll = (itemContent.getChildAt(1) as ViewGroup)
+                                                            for (m in 0 until lll.childCount) {
+                                                                val comItem = (lll.getChildAt(m) as ViewGroup)
+                                                                val ll = comItem.getChildAt(0) as ViewGroup
+                                                                ll.background = getTransparentRippleDrawable()
+                                                                // 去掉分割线
+                                                                ll.getChildAt(0).background = transparentDrawable
+                                                            }
+                                                        }
+                                                    }
+                                                }
                                             }
                                         }
                                     }
