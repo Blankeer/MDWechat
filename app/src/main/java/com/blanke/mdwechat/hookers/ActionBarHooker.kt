a8047b227715ac0c9cca2d27301e454daecc95b9
Blanke <blankeeee@gmail.com>
2018-07-01 09:52:06 +0800

    [REM] remove no use code.

--- app/src/main/java/com/blanke/mdwechat/hookers/ActionBarHooker.kt
+++ app/src/main/java/com/blanke/mdwechat/hookers/ActionBarHooker.kt
@@ -3,17 +3,11 @@ package com.blanke.mdwechat.hookers
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.graphics.drawable.Drawable
-import android.view.View
 import android.view.ViewGroup
-import android.widget.FrameLayout
-import android.widget.ImageView
-import android.widget.LinearLayout
 import com.blanke.mdwechat.Classes.ActionBarContainer
 import com.blanke.mdwechat.Objects.Main.LauncherUI_mActionBarContainer
 import com.blanke.mdwechat.WeChatHelper.colorPrimaryDrawable
-import com.blanke.mdwechat.WeChatHelper.defaultImageRippleBorderDrawable
 import com.blanke.mdwechat.config.HookConfig
-import com.blanke.mdwechat.util.ViewUtils
 import com.gh0u1l5.wechatmagician.spellbook.base.Hooker
 import com.gh0u1l5.wechatmagician.spellbook.base.HookerProvider
 import com.gh0u1l5.wechatmagician.spellbook.mirror.com.tencent.mm.ui.Classes.LauncherUI
@@ -51,27 +45,6 @@ object ActionBarHooker : HookerProvider {
                             actionBar.elevation = 0F
                         }
                     }
-                }
-            }
-        })
-        // remove split ImageView in ActionBar
-        findAndHookMethod(ImageView::class.java, "onAttachedToWindow", object : XC_MethodHook() {
-            override fun afterHookedMethod(param: MethodHookParam?) {
-                val iv = param?.thisObject as ImageView
-                val frameLayout = ViewUtils.getParentView(iv, 4)
-                val ivParent = ViewUtils.getParentView(iv, 1)
-                if (frameLayout != null
-                        && frameLayout is FrameLayout
-                        && ivParent is LinearLayout
-                        && ivParent.childCount == 2
-                        && ivParent.indexOfChild(iv) == 1
-                        && ivParent.getChildAt(0) is ImageView) {
-//                    log("actionbar frameLayout =$frameLayout")
-                    iv.visibility = View.GONE
-
-                    val backIv = ivParent.getChildAt(0) as ImageView
-                    backIv.background = defaultImageRippleBorderDrawable
-                    ivParent.background = null
                 }
             }
         })
