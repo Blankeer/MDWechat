bc77cc51576a96b427f6f9d59ddb93f3badd6816
Blanke <blankeeee@gmail.com>
2019-01-06 10:04:53 +0800

    [FIX] 修复主界面 actionBar 阴影显示问题

--- app/src/main/java/com/blanke/mdwechat/hookers/ActionBarHooker.kt
+++ app/src/main/java/com/blanke/mdwechat/hookers/ActionBarHooker.kt
@@ -12,6 +12,7 @@ import com.blanke.mdwechat.WechatGlobal
 import com.blanke.mdwechat.hookers.base.Hooker
 import com.blanke.mdwechat.hookers.base.HookerProvider
 import de.robv.android.xposed.XC_MethodHook
+import de.robv.android.xposed.XposedHelpers
 import de.robv.android.xposed.XposedHelpers.findAndHookMethod
 
 object ActionBarHooker : HookerProvider {
@@ -25,6 +26,7 @@ object ActionBarHooker : HookerProvider {
             override fun beforeHookedMethod(param: MethodHookParam) {
                 val drawable = param.args[0]
                 var needHook = true
+                val actionBar = param.thisObject as ViewGroup
                 if (drawable is ColorDrawable) {
 //                    LogUtil.log("actionbar color=" + Integer.toHexString(drawable.color))
                     if (drawable.color == Color.parseColor("#F2F2F2")
@@ -32,17 +34,23 @@ object ActionBarHooker : HookerProvider {
                             || drawable.color == Color.TRANSPARENT) {
                         needHook = false
                     }
-                }
-                val actionBar = param.thisObject as ViewGroup
-                if (WechatGlobal.wxVersion!! >= Version("7.0.0")) {
-                    if (actionBar.context::class.java.name == Classes.LauncherUI.name) {
-                        needHook = true
+                    if (WechatGlobal.wxVersion!! >= Version("7.0.0")) {
+                        if (actionBar.context::class.java.name == Classes.LauncherUI.name) {
+                            needHook = true
+                        }
+                    }
+                    if (drawable.color == colorPrimaryDrawable.color) {
+                        needHook = false
                     }
                 }
                 if (needHook) {
                     param.args[0] = colorPrimaryDrawable
                 }
-                actionBar.elevation = 5F
+                val init_elevation = XposedHelpers.getAdditionalInstanceField(actionBar, "init_elevation")
+                if (init_elevation == null) {
+                    actionBar.elevation = 5F
+                    XposedHelpers.setAdditionalInstanceField(actionBar, "init_elevation", true)
+                }
             }
         })
     }
